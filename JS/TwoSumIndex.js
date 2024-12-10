4ac4f749c5789721f51a98bf5859b8ccf9b66ea4
naamyfatimazahra <naamyfatimazahra@gmail.com>
2024-12-10 09:13:20 +0100

    solve the problem about check if there is duplicate nums return true if not return false

--- JS/TwoSumIndex.js
+++ JS/TwoSumIndex.js
@@ -30,3 +30,5 @@ function TwoSumIndex(arr, target) {
   }
 }
 // return [0,3]
+
+
---
+++ JS/containsDuplicate.js
@@ -0,0 +1,12 @@
+console.log(containsDuplicate([0, 2, 4, 1]));
+
+function containsDuplicate(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[i] === arr[j]) {
+        return true;
+      }
+    }
+  }
+  return false;
+}
