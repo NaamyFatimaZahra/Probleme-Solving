9c9f36be959d99bb0f51cebe034a4c9fa8558499
naamyfatimazahra <naamyfatimazahra@gmail.com>
2025-04-29 17:00:43 +0100

    sorting without function

---
+++ JS/sort.js
@@ -0,0 +1,11 @@
+let arr = ["abid", "aaid", "boom", "bob", "d"];
+
+for (let i = 0; i < arr.length; i++) {
+  for (let j = 0; j < arr.length; j++) {
+    if (arr[j] > arr[j + 1]) {
+      [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+    }
+  }
+}
+
+console.log(arr);
